111099cab2d621c964e3244c1286454f791c3bd4
Marianne Borowiak <marianneb@nulogy.com>
2020-01-24 12:37:27 -0500

    format app.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,45 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import {Box, NDSProvider, NavBar, theme} from "@nulogy/components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Box, NDSProvider, NavBar, theme } from "@nulogy/components";
 import { Home } from "./pages/Home";
 import { Details } from "./pages/Details";
 import { Custom } from "./pages/Custom";
 
 const App = () => {
   const primaryMenu = [
-    { name: 'Home',  render:  () => <Link to="/">Home</Link>},
-    { name: 'Details',  render:  () => <Link to="/details">Details</Link>},
-    { name: 'Custom',  render:  () => <Link to="/custom">Custom</Link>},
+    { name: "Home", render: () => <Link to="/">Home</Link> },
+    { name: "Details", render: () => <Link to="/details">Details</Link> },
+    { name: "Custom", render: () => <Link to="/custom">Custom</Link> }
   ];
-  
+
   const secondaryMenu = [
     {
-      name: 'Settings',
+      name: "Settings",
       items: [
-        { name: 'Permissions', href: '/' },
-        { name: 'Manage account', href: '/' },
-      ],
-    },
+        { name: "Permissions", href: "/" },
+        { name: "Manage account", href: "/" }
+      ]
+    }
   ];
-  
+
   const search = {
-    onSubmit: () => {},
+    onSubmit: () => {}
   };
-  
+
   return (
     <Router>
       <NDSProvider theme={theme}>
-        <NavBar menuData={ { primaryMenu, secondaryMenu, search } } />
+        <NavBar menuData={{ primaryMenu, secondaryMenu, search }} />
         <Box p="x6">
           <Switch>
             <Route exact path="/">
-              <Home/>
+              <Home />
             </Route>
             <Route path="/details">
-              <Details/>
+              <Details />
             </Route>
             <Route path="/custom">
-              <Custom/>
+              <Custom />
             </Route>
           </Switch>
         </Box>
