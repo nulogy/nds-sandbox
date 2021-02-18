ddd0c9323c0d1583f72b0ced258adbbb10645881
Marianne Borowiak <marianneb@nulogy.com>
2021-02-18 11:31:16 -0500

    Update nds navbar and fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { Box, NDSProvider, NavBar, theme } from "@nulogy/components";
+import { Box, NDSProvider, BrandedNavBar, theme } from "@nulogy/components";
 import { primaryMenu, secondaryMenu, search } from "./data/menuData.js";
 import { Pages } from "./pages/index";
 
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Router>
       <NDSProvider theme={theme}>
-        <NavBar menuData={{ primaryMenu, secondaryMenu, search }} />
+        <BrandedNavBar menuData={{ primaryMenu, secondaryMenu, search }} />
         <Box>
           <Pages />
         </Box>
