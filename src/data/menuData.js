161a8783a8c1fc925c81c9f06c9d63882636381b
Marianne Borowiak <marianneb@nulogy.com>
2021-02-19 14:53:18 -0500

    Add charting library and example

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export const primaryMenu = [
   { name: "Home", render: () => <Link to="/">Home</Link> },
   { name: "Details", render: () => <Link to="/details">Details</Link> },
+  { name: "Charts", render: () => <Link to="/charts">Charts</Link> },
   { name: "Custom", render: () => <Link to="/custom">Custom</Link> }
 ];
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,24 @@ import { Home } from "./Home";
 import { Details } from "./Details";
 import { Custom } from "./Custom";
 /* Import routes here */
+import { Charts } from "./Charts";
 
 export const Pages = () => {
   return (
     <Switch>
-      <Route exact path="/"><Home /></Route>
-      <Route path="/details"><Details /></Route>
-      <Route path="/custom"><Custom /></Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/details">
+        <Details />
+      </Route>
+      <Route path="/custom">
+        <Custom />
+      </Route>
       {/* Add routes here */}
+      <Route path="/charts">
+        <Charts />
+      </Route>
     </Switch>
   );
 };
